d8b27eda2b0ebbf4754b3afa36cd63f9856deea0
Pavel Barysenka <pavel_barysenka@epam.com>
2025-09-24 18:27:46 +0200

    create initial basetest

---
+++ src/test/java/com/pavel/qa/base/BaseTest.java
@@ -0,0 +1,23 @@
+package com.pavel.qa.base;
+
+import io.qameta.allure.testng.AllureTestNg;
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Listeners({AllureTestNg.class})
+public class BaseTest {
+
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @BeforeClass
+    public void setup() {
+        // Base URI can be configured via system property or config file
+        String baseUrl = System.getProperty("baseUrl", "http://3.68.165.45");
+        RestAssured.baseURI = baseUrl;
+        logger.info("Base URI set to: {}", baseUrl);
+    }
+}
+
