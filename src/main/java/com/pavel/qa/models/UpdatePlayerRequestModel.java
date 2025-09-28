acdb217405ef0461f64829ed63ffec65f64613f0
Pavel Barysenka <pavel_barysenka@epam.com>
2025-09-28 12:16:17 +0200

    create models for update player request and response

---
+++ src/main/java/com/pavel/qa/models/UpdatePlayerRequestModel.java
@@ -0,0 +1,59 @@
+package com.pavel.qa.models;
+
+public class UpdatePlayerRequestModel {
+    private Integer age;
+    private String gender;
+    private String login;
+    private String password;
+    private String role;
+    private String screenName;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getScreenName() {
+        return screenName;
+    }
+
+    public void setScreenName(String screenName) {
+        this.screenName = screenName;
+    }
+
+}
---
+++ src/main/java/com/pavel/qa/models/UpdatePlayerResponseModel.java
@@ -0,0 +1,59 @@
+package com.pavel.qa.models;
+
+public class UpdatePlayerResponseModel {
+    private Integer age;
+    private String gender;
+    private Long id;
+    private String login;
+    private String role;
+    private String screenName;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getScreenName() {
+        return screenName;
+    }
+
+    public void setScreenName(String screenName) {
+        this.screenName = screenName;
+    }
+
+}
